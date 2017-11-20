1165b60fcea2a2200887750e1b02d4781b7a6b35
Don Pinkus <don.pinkus@gmail.com>
2017-11-20 10:21:46 -0800

    smart-rockets

---
+++ good/canvas-spinning-paint-wheel/main.js
@@ -0,0 +1,134 @@
+let g = {
+  mousePos: {
+    x: 0,
+    y: 0
+  },
+  drawCanvas: null,
+  drawCtx: null,
+  viewCanvas: null,
+  viewCtx: null,
+  rotationAngle: 9,
+  t: 0,
+  intensity: 0,
+  intensityAscending: true,
+  canvasWidth:
+    (window.innerWidth > window.innerHeight
+      ? window.innerHeight
+      : window.innerWidth) * 1.5,
+  canvasHeight:
+    (window.innerWidth > window.innerHeight
+      ? window.innerHeight
+      : window.innerWidth) * 1.5,
+  colorIndex: 0,
+  inputs: {
+    speed: null,
+    color: null
+  }
+};
+
+/**
+ * A linear interpolator for hexadecimal colors
+ * @param {String} a
+ * @param {String} b
+ * @param {Number} amount
+ * @example
+ * // returns #7F7F7F
+ * lerpColor('#000000', '#ffffff', 0.5)
+ * @returns {String}
+ */
+function lerpColor(a, b, amount) {
+  var ah = parseInt(a.replace(/#/g, ""), 16),
+    ar = ah >> 16,
+    ag = (ah >> 8) & 0xff,
+    ab = ah & 0xff,
+    bh = parseInt(b.replace(/#/g, ""), 16),
+    br = bh >> 16,
+    bg = (bh >> 8) & 0xff,
+    bb = bh & 0xff,
+    rr = ar + amount * (br - ar),
+    rg = ag + amount * (bg - ag),
+    rb = ab + amount * (bb - ab);
+
+  return (
+    "#" + (((1 << 24) + (rr << 16) + (rg << 8) + rb) | 0).toString(16).slice(1)
+  );
+}
+
+document.addEventListener("DOMContentLoaded", onDocumentReady);
+document.addEventListener("mousemove", onMouseMove);
+
+function onDocumentReady(e) {
+  // canvases
+  g.drawCanvas = document.getElementById("drawCanvas");
+  g.viewCanvas = document.getElementById("viewCanvas");
+
+  g.drawCtx = g.drawCanvas.getContext("2d");
+  g.viewCtx = g.viewCanvas.getContext("2d");
+
+  g.drawCanvas.width = g.viewCanvas.width = g.canvasWidth;
+  g.drawCanvas.height = g.viewCanvas.height = g.canvasHeight;
+
+  // Translate origins
+  g.drawCtx.translate(g.drawCanvas.width / 2, g.drawCanvas.height / 2);
+  g.viewCtx.translate(g.drawCanvas.width / 2, g.drawCanvas.height / 2);
+
+  draw();
+}
+
+function onMouseMove(e) {
+  g.mousePos = {
+    x: e.clientX,
+    y: e.clientY
+  };
+}
+
+function draw() {
+  g.t++;
+
+  // Draw on canvas
+  const transX = g.mousePos.x - window.innerWidth / 2;
+  const transY = g.mousePos.y - window.innerHeight / 2;
+
+  const radianAngle = g.rotationAngle * Math.PI / 180 * g.t * -1;
+
+  // Add inverseRotation to mouse position.
+  const drawX = transX * Math.cos(radianAngle) - transY * Math.sin(radianAngle);
+
+  const drawY = transY * Math.cos(radianAngle) + transX * Math.sin(radianAngle);
+
+  // Get color
+  if (g.intensity === 99) {
+    g.intensityAscending = false;
+  } else if (g.intensity === 1) {
+    g.intensityAscending = true;
+  }
+
+  g.intensityAscending ? g.intensity++ : g.intensity--;
+
+  const colors = ["#FF0080", "#FF8C00"];
+
+  const color = lerpColor(
+    colors[g.colorIndex],
+    colors[g.colorIndex + 1],
+    g.intensity / 100
+  );
+
+  g.drawCtx.fillStyle = color;
+  g.drawCtx.fillRect(drawX, drawY, 20, 20);
+
+  // Copy drawCtx to viewCtx.
+  g.viewCtx.rotate(Math.PI / 180 * g.rotationAngle);
+  g.viewCtx.drawImage(
+    g.drawCanvas,
+    0, // sourceX
+    0, // sourceY - note that source ignores translation. It's not a canvas context, so we choose top left corner of the canvas to start copying pixels.
+    g.drawCanvas.width, // sourceWidth
+    g.drawCanvas.height, // sourceHeight
+    -0.5 * g.viewCanvas.width, // destinationX
+    -0.5 * g.viewCanvas.height, // destinationY
+    g.viewCanvas.width, // destinationWidth
+    g.viewCanvas.height // destinationHeight
+  );
+
+  requestAnimationFrame(draw);
+}
--- randos/processing/smart-rockets/main.js
+++ randos/processing/smart-rockets/main.js
@@ -1,29 +1,28 @@
 var rocket;
 var population;
-var lifespan = 200;
+var lifespan = 300;
 var lifeP;
 var maxFitP;
 var minFitP;
 var count = 0;
 var target;
-var popSize = 100;
 
-var gWidth = window.innerWidth;
-var gHeight = window.innerHeight;
+var rx = 100;
+var ry = 150;
+var rw = 200;
+var rh = 10;
 
 function setup() {
-  createCanvas(gWidth, gHeight);
+  createCanvas(500, 500);
   rocket = new Rocket();
   population = new Population();
   lifeP = createP();
   maxFitP = createP();
-  target = createVector(gWidth / 2, gHeight / 6);
-
-  background(40);
+  target = createVector(width / 2, 50);
 }
 
 function draw() {
-  background(0, 0.9);
+  background(0);
   population.run();
   lifeP.html(count);
 
@@ -34,13 +33,17 @@ function draw() {
     count = 0;
   }
 
-  fill("rgb(249, 38, 114)");
+  // obstacle
+  fill(255);
+  rect(rx, ry, rw, rh);
+
+  fill(255);
   ellipse(target.x, target.y, 16, 16);
 }
 
 function Population() {
   this.rockets = [];
-  this.popsize = window.popSize;
+  this.popsize = 500;
   this.matingPool = [];
 
   for (var i = 0; i < this.popsize; i++) {
@@ -67,7 +70,7 @@ function Population() {
     this.matingPool = [];
     for (var i = 0; i < this.popsize; i++) {
       // Add fit rockets much more.
-      var n = this.rockets[i].fitness * 1000;
+      var n = this.rockets[i].fitness * 100;
       for (var j = 0; j < n; j++) {
         this.matingPool.push(this.rockets[i]);
       }
@@ -129,7 +132,7 @@ function DNA(genes) {
 
   this.mutate = function() {
     for (var i = 00; i < this.genes.length; i++) {
-      if (Math.random() < 0.01) {
+      if (Math.random() < 0.05) {
         this.genes[i] = p5.Vector.random2D();
         this.genes[i].setMag(0.1);
       }
@@ -138,7 +141,7 @@ function DNA(genes) {
 }
 
 function Rocket(dna) {
-  this.pos = createVector(width / 2, height / 6 * 5);
+  this.pos = createVector(width / 2, height);
   this.vel = createVector();
   this.acc = createVector();
   this.crashed = false;
@@ -157,28 +160,52 @@ function Rocket(dna) {
   };
 
   this.calcFitness = function() {
-    var d = 1 / dist(this.pos.x, this.pos.y, target.x, target.y);
+    var d = dist(this.pos.x, this.pos.y, target.x, target.y);
 
-    this.fitness = Math.pow(d, 10);
+    this.fitness = map(d, 0, width, width, 0);
+
+    if (this.crashed) {
+      this.fitness = 1;
+    }
+
+    this.fitness = Math.pow(this.fitness, 2);
   };
 
   this.update = function() {
+    const hitObstacle =
+      this.pos.x > rx &&
+      this.pos.x < rx + rw &&
+      this.pos.y > ry &&
+      this.pos.y < ry + rh;
+
+    const hitWall =
+      this.pos.x > width ||
+      this.pos.x < 0 ||
+      this.pos.y > height ||
+      this.pos.y < 0;
+
+    if (hitObstacle || hitWall) {
+      this.crashed = true;
+    }
+
     this.applyForce(this.dna.genes[count]);
 
-    this.vel.add(this.acc);
-    this.pos.add(this.vel);
-    this.acc.mult(0);
+    if (!this.crashed) {
+      this.vel.add(this.acc);
+      this.pos.add(this.vel);
+      this.acc.mult(0);
+    }
   };
 
   this.show = function() {
     push(); // rotating and translating wont effect other objects
     noStroke();
-    fill(255);
+    fill(255, 80);
     translate(this.pos.x, this.pos.y);
 
     rotate(this.vel.heading()); // Gives angle vector is pointing
     rectMode(CENTER);
-    rect(0, 0, 5, 2);
+    rect(0, 0, 25, 5);
     pop();
   };
 }
