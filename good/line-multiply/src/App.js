a1c80f8a4171fc518e6320e29fa72dba78eb3b86
Don Pinkus <don.pinkus@gmail.com>
2017-11-21 09:37:15 -0800

    initial content

--- good/line-multiply/src/App.js
+++ good/line-multiply/src/App.js
@@ -1,18 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Header">
+          <h1>Multiple Two Numbers</h1>
+          <div className="NumberInputs">
+            <input className="NumberInputs-Input" type="number" value="123" />
+            <span>x</span>
+            <input className="NumberInputs-Input" type="number" value="234" />
+            <span>=</span>
+            <span>28,782</span>
+          </div>
+        </div>
+
+        <div>
+          <p>Count the intersections in each column.</p>
+          <div className="Columns">
+            <div className="Columns-Column">
+              <p className="Columns-Count">2</p>
+              <p className="Columns-Multiplier">x10,000</p>
+            </div>
+            <div className="Columns-Column">
+              <p className="Columns-Count">7</p>
+              <p className="Columns-Multiplier">x1,000</p>
+            </div>
+            <div className="Columns-Column">
+              <p className="Columns-Count">16</p>
+              <p className="Columns-Multiplier">x100</p>
+            </div>
+            <div className="Columns-Column">
+              <p className="Columns-Count">17</p>
+              <p className="Columns-Multiplier">x10</p>
+            </div>
+            <div className="Columns-Column">
+              <p className="Columns-Count">12</p>
+              <p className="Columns-Multiplier">x1</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
